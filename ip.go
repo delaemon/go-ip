d414ee09fe4dcb9b5a1bb38e4be99692f4e52f9a
delaemon <takeshi.music.5@gmail.com>
2015-11-13 23:33:19 +0900

    Add log, connection limit

--- ip.go
+++ ip.go
@@ -6,16 +6,72 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/exec"
+	"strconv"
 	"time"
+
+	"golang.org/x/net/netutil"
+
+	"github.com/alecthomas/template"
 )
 
 var (
 	d = flag.Bool("d", false, "Whether or not to launch in the background(like a daemon)")
 	p = flag.String("p", "8080", "Linten port")
+	c = flag.String("c", "100", "Client connection")
 )
+
+var (
+	accessTime        = time.Now()
+	accessLogTemplate = `{{.RemoteAddr}} {{.ContentType}} {{.Method}} {{.Path}} {{.Query}} {{.Body}} {{.UserAgent}}`
+)
+
+type accessLogLine struct {
+	RemoteAddr  string
+	ContentType string
+	Path        string
+	Query       string
+	Method      string
+	Body        string
+	UserAgent   string
+}
+
+func accessLog(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		bufbody := new(bytes.Buffer)
+		bufbody.ReadFrom(req.Body)
+		body := bufbody.String()
+		line := accessLogLine{
+			req.RemoteAddr,
+			req.Header.Get("Content-Type"),
+			req.URL.Path,
+			req.URL.RawQuery,
+			req.Method, body, req.UserAgent(),
+		}
+		tmpl, err := template.New("line").Parse(accessLogTemplate)
+		if err != nil {
+			panic(err)
+		}
+		bufline := new(bytes.Buffer)
+		err = tmpl.Execute(bufline, line)
+		if err != nil {
+			panic(err)
+		}
+
+		logFile := fmt.Sprintf("./log/access/%d%02d%02d.log", accessTime.Year(), accessTime.Month(), accessTime.Day())
+		f, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			panic(err)
+		}
+		log.SetOutput(f)
+		log.Printf(bufline.String())
+
+		handler.ServeHTTP(w, req)
+	})
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s %s \"%s %s %s %s\" %d %s %s\n",
@@ -30,32 +86,56 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		r.UserAgent())
 }
 
+func server() {
+	cmd := exec.Command(os.Args[0], "-p", *p, "-c", *c)
+	serr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = cmd.Start()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	s, err := ioutil.ReadAll(serr)
+	s = bytes.TrimSpace(s)
+	if bytes.HasPrefix(s, []byte("addr: ")) {
+		fmt.Println(string(s))
+		cmd.Process.Release()
+	} else {
+		log.Printf("unexpected response: `%s` error: `%v`\n", s, err)
+		cmd.Process.Kill()
+	}
+}
+
+func client() {
+	http.HandleFunc("/", handler)
+	listener, err := net.Listen("tcp", ":"+*p)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	con, err := strconv.Atoi(*c)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	limit_listener := netutil.LimitListener(listener, con)
+	http_config := &http.Server{
+		Handler: accessLog(http.DefaultServeMux),
+	}
+	defer limit_listener.Close()
+	err = http_config.Serve(limit_listener)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
 func init() {
 	flag.Parse()
 }
 
 func main() {
 	if *d {
-		cmd := exec.Command(os.Args[0], "-p", *p)
-		serr, err := cmd.StderrPipe()
-		if err != nil {
-			log.Fatalln(err)
-		}
-		err = cmd.Start()
-		if err != nil {
-			log.Fatalln(err)
-		}
-		s, err := ioutil.ReadAll(serr)
-		s = bytes.TrimSpace(s)
-		if bytes.HasPrefix(s, []byte("addr: ")) {
-			fmt.Println(string(s))
-			cmd.Process.Release()
-		} else {
-			log.Printf("unexpected response: `%s` error: `%v`\n", s, err)
-			cmd.Process.Kill()
-		}
+		server()
 	} else {
-		http.HandleFunc("/", handler)
-		http.ListenAndServe(":"+*p, nil)
+		client()
 	}
 }
